46971b509338811b2924bc54723c0c631d665b1f
schifzt <tagashira.fumito@gmail.com>
2020-10-05 05:18:35 +0900

    delete save state functional & add confirm reload notification

--- src/pdfjs/web/custom.js
+++ src/pdfjs/web/custom.js
@@ -1,4 +1,7 @@
-var hideOpenFileButton = function () {
+window.addEventListener('load', evt => {
     var openFile = document.getElementById("openFile");
-    console.log("hello");
-}
+    var parent = openFile.parentElement;
+    openFile.addEventListener('blur', evt => {
+        parent.removeChild(openFile);
+    });
+});
